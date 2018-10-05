41a7c846519ab350cfd377d47fb5c91115191c66
maicolsilver <maicolsilver94@hotmil.es>
2018-10-05 10:24:39 -0400

    hola mundo

---
+++ Informatica/includes/header.php
@@ -0,0 +1,33 @@
+<header>
+	<nav class="navbar navbar-default navbar-fixed-top">
+  		<div class="container">
+			<div class="navbar-header">
+			    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-navbar" aria-expanded="false">
+				    <span class="sr-only">Toggle navigation</span>
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+			    </button>
+	      		<a class="logo"  href="solicitud.php">
+	      			<img src="../images/CSJ-SIN-FONDO.png" width="90px" margin-top="-5px">
+	      		</a>
+	    	</div>
+			<div class="collapse navbar-collapse" id="menu-navbar">
+			    <ul class="nav navbar-nav navbar-right">
+			        <li><a href="solicitud.php">Inicio</a></li>
+			        <li><a href="#">Mis Servicios</a></li>
+			        <li><a href="#">Terminados</a></li>
+			        <li><a href="#">Pendientes</a></li>
+			        <li class="dropdown">
+			         	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+			         		Contenido <span class="caret"></span>
+			         	</a>
+			         	<ul class="dropdown-menu">
+			            	<li><a href="#">Cerrar Sesion</a></li>
+			          	</ul>
+			        </li>
+			    </ul>
+			</div>
+		</div>
+	</nav>
+</header>
---
+++ Informatica/solicitud.php
@@ -0,0 +1,50 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Solicitud - Servicios</title>
+    <meta name="description" content="">
+    <?php include 'includes/head.php'; ?>
+  </head>
+
+  <body>
+      <?php include 'includes/header.php'; ?>
+
+      <main>
+          <section class="section-info">
+              <div class="container">
+                  <h2 class="title">Solicitud de Asistencia Técnica</h2>
+              </div>
+          </section>
+          <section class="section-info">
+            <div class="container">
+              <div class="row">
+                  <div class="col-sm-3">
+                      <div class="box-info">
+                          <h4>Funcionario: Maicol Silvera</h4>
+
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <div class="box-info">
+
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <div class="box-info">
+
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <div class="box-info">
+
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </section>
+      </main>
+
+      <?php include 'includes/script.php'; ?>
+  </body>
+</html>
